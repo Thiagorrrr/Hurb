b625b4ff1c6b26e07fb338de1d880addf5069944
Thiagorrrr <thiago.pereira.053@gmail.com>
2021-03-22 02:20:55 -0300

    ajuse case render

--- src/App.js
+++ src/App.js
@@ -6,28 +6,34 @@ import './Styles/Scss/App.scss'
 
 function App() {
   const [data, setData] = useState([]);
-  const [hasError, setHasError] = useState(false);
-  const host = 'https://www.bing.com/'; 
+  const host = 'https://www.bing.com/';
   const divStyle = {
-    backgroundImage:`url('${host}${data.url})`,
+    backgroundImage: `url('${host}${data?.url})`,
   };
 
   useEffect(() => {
     const url = `${host}HPImageArchive.aspx?format=js&idx=0&n=1&mkt=pt-BR`;
-    
+
     fetch(url)
       .then(response => response.json())
-      .then(data => setData( data.images[0] ))
-      .catch(err => setHasError(true))
+      .then(data => setData(data.images[0]))
+      .catch(err => console.log(err, 'error'))
   }, [])
   
   return (
     <>
-      { hasError !== true && data.url ?
-        <main className="main" style={divStyle}>
-          <Header/>
-          <Weather/>
-        </main> : null
+      {
+        data?.url ?
+
+          <main className="main" style={divStyle}>
+            <Header />
+            <Weather />
+          </main>
+          :
+          <main className="main">
+            <Header />
+            <Weather />
+          </main>
       }
     </>
   )
